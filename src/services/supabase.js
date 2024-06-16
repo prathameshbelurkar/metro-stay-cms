6d610693cfd1a9b87d3e8b4d9c3bcd80579aaca5
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2024-06-16 18:37:20 +0530

    supabase: migrated base api url to env

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://hoimwhyialuigwgxbozm.supabase.co";
+const supabaseUrl = import.meta.env.VITE_BASE_URL;
 const supabaseKey = import.meta.env.VITE_API_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
