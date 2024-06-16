8c2d236ba6a77379a522a4ebfed0dff1d01985af
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2024-06-16 11:22:38 +0530

    chore: re-usable code

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,32 @@
+import styled from "styled-components";
+
+import GlobalStyles from "./styles/GlobalStyles";
+
+import Heading from "./ui/Heading";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+
+const StyledApp = styled.div`
+  background-color: limegreen;
+  padding: 20px;
+`;
+
 function App() {
-  return <p>Hello World</p>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Heading as="h1">Modern Stay</Heading>
+
+        <Heading as="h2">Check in and out</Heading>
+        <Button onClick={() => alert("Checked In")}>Check In</Button>
+        <Button onClick={() => alert("Checked Out")}>Check Out</Button>
+
+        <Heading as="h3">Form</Heading>
+        <Input type="number" placeholder="Number of guests" />
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;
