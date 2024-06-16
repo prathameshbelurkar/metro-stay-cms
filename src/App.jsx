4c6049d1e4b695fed155d6f1c6d980cbad5201ed
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2024-06-16 11:46:03 +0530

    chore: config button and row component

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ import GlobalStyles from "./styles/GlobalStyles";
 import Heading from "./ui/Heading";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
+import Row from "./ui/Row";
 
 const StyledApp = styled.div`
-  background-color: limegreen;
   padding: 20px;
 `;
 
@@ -16,14 +16,30 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <Heading as="h1">Modern Stay</Heading>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">Modern Stay</Heading>
+            <div>
+              <Heading as="h2">Check in and out</Heading>
+              <Button onClick={() => alert("Checked In")}>Check In</Button>
+              <Button
+                variation="secondary"
+                size="small"
+                onClick={() => alert("Checked Out")}
+              >
+                Check Out
+              </Button>
+            </div>
+          </Row>
 
-        <Heading as="h2">Check in and out</Heading>
-        <Button onClick={() => alert("Checked In")}>Check In</Button>
-        <Button onClick={() => alert("Checked Out")}>Check Out</Button>
-
-        <Heading as="h3">Form</Heading>
-        <Input type="number" placeholder="Number of guests" />
+          <Row>
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input type="number" placeholder="Number of guests" />
+              <Input type="number" placeholder="Number of guests" />
+            </form>
+          </Row>
+        </Row>
       </StyledApp>
     </>
   );
