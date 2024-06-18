061a92a808cf37007393bef180c5af1e465ee013
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2024-06-18 16:00:56 +0530

    upload-storage: added api handler

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,5 +1,7 @@
 import supabase from "./supabase";
 
+import { supabaseUrl } from "./supabase";
+
 export async function getCabins() {
   const { data, error } = await supabase.from("cabins").select("*");
   if (error) {
@@ -10,11 +12,35 @@ export async function getCabins() {
 }
 
 export async function createCabin(newCabin) {
-  const { data, error } = await supabase.from("cabins").insert([newCabin]);
+  const imageName = `${Math.random()}-${encodeURIComponent(
+    newCabin.image.name.replaceAll("/", "")
+  )}`;
+
+  // eslint-disable-next-line
+  const imagePath = `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
+
+  const { data, error } = await supabase
+    .from("cabins")
+    .insert([{ ...newCabin, image: imagePath }]);
   if (error) {
     console.error(error);
     throw new Error("Cabins could not be loaded");
   }
+
+  // Upload image
+  const { error: storageError } = await supabase.storage
+    .from("cabin-images")
+    .upload(imageName, newCabin.image);
+
+  // Delete the cabin if there was an error uploading image
+  if (storageError) {
+    await supabase.from("cabins").delete().eq("id", data.id);
+    console.error(storageError);
+    throw new Error(
+      "Image could not be uploaded and the cabin was not created."
+    );
+  }
+
   return data;
 }
 
