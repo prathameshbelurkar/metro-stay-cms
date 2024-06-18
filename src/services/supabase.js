74d7af6a1be2b1d29d22e06d89317cff98985c36
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2024-06-18 16:01:14 +0530

    export: base API url

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_BASE_URL;
+export const supabaseUrl = import.meta.env.VITE_BASE_URL;
 const supabaseKey = import.meta.env.VITE_API_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
