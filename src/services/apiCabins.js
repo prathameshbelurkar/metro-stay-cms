8afaeb32d7a8d427c2032a832db29455fae80e7b
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2024-06-18 07:30:02 +0530

    fetch-fn: added create fn for cabin

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -9,6 +9,15 @@ export async function getCabins() {
   return data;
 }
 
+export async function createCabin(newCabin) {
+  const { data, error } = await supabase.from("cabins").insert([newCabin]);
+  if (error) {
+    console.error(error);
+    throw new Error("Cabins could not be loaded");
+  }
+  return data;
+}
+
 export async function deleteCabin(id) {
   const { data, error } = await supabase.from("cabins").delete().eq("id", id);
   if (error) {
