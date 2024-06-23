d5d38d82c87c36e8034e51bd70754bae262eac0c
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2024-06-23 15:58:42 +0530

    feat: sorting for '/bookings' (#9)

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -13,13 +13,18 @@ export function useBookings() {
       ? null
       : { field: "status", value: filterValue, method: "eq" };
 
+  // SORT
+  const sortByRaw = searchParams.get("sortBy") || "startDate-desc";
+  const [field, direction] = sortByRaw.split("-");
+  const sortBy = { field, direction };
+
   const {
     isLoading,
     error,
     data: bookings,
   } = useQuery({
-    queryKey: ["bookings", filter],
-    queryFn: () => getBookings({ filter, sortBy: null }),
+    queryKey: ["bookings", filter, sortBy],
+    queryFn: () => getBookings({ filter, sortBy }),
   });
 
   return { isLoading, error, bookings };
--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -7,10 +7,13 @@ export async function getBookings({ filter, sortBy }) {
   let query = supabase.from("bookings").select(queryStr);
 
   // FILTER
-  if (filter !== null)
-    query = query[filter.method || "eq"](filter.field, filter.value);
+  if (filter) query = query[filter.method || "eq"](filter.field, filter.value);
 
   // SORT
+  if (sortBy)
+    query = query.order(sortBy.field, {
+      ascending: sortBy.direction === "asc",
+    });
 
   const { data, error } = await query;
 
