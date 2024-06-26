b9aa2b66fa52b565b8e13cc0135853603575c7b3
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2024-06-26 18:21:42 +0530

    api: add service for login (#18)

---
+++ src/features/authentication/useLogin.js
@@ -0,0 +1,25 @@
+import { useNavigate } from "react-router-dom";
+import { useMutation } from "@tanstack/react-query";
+import { toast } from "react-hot-toast";
+
+import { login as loginApi } from "../../services/apiAuth";
+
+export function useLogin() {
+  const navigate = useNavigate();
+
+  const {
+    mutate: login,
+    isLoading,
+    error,
+  } = useMutation({
+    mutationFn: ({ email, password }) => loginApi({ email, password }),
+    onSuccess: () => {
+      navigate("/dashboard");
+    },
+    onError: () => {
+      toast.error("Provided email or password are incorrect");
+    },
+  });
+
+  return { login, isLoading, error };
+}
