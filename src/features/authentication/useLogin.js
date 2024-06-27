52984b179f1824812499b402eeeab47a26d9936b
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2024-06-27 13:02:14 +0530

    optimization: replace key in navigate hook (#20)

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -16,7 +16,7 @@ export function useLogin() {
     mutationFn: ({ email, password }) => loginApi({ email, password }),
     onSuccess: (user) => {
       queryClient.setQueriesData(["user"], user);
-      navigate("/dashboard");
+      navigate("/dashboard", { replace: true });
     },
     onError: () => {
       toast.error("Provided email or password are incorrect");
