2ebfdc1ead19846f2d421eabb129c7024e058cde
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2024-06-28 14:09:32 +0530

    optimization: add cloud storage path (#20)

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -4,4 +4,6 @@ export const supabaseUrl = import.meta.env.VITE_BASE_URL;
 const supabaseKey = import.meta.env.VITE_API_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
+export const supabaseStoragePath = import.meta.env.VITE_STORAGE_PATH;
+
 export default supabase;
