52c16e65cbcdb3ab204fd86075c681ee06cd435e
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2024-06-29 07:35:38 +0530

    api: added hooks for fetching dashboard bookings and stays (#25)

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,5 +1,10 @@
 import styled from "styled-components";
 
+import Spinner from "../../ui/Spinner";
+
+import { useRecentBookings } from "./useRecentBookings";
+import { useRecentStays } from "./useRecentStays";
+
 const StyledDashboardLayout = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
@@ -8,6 +13,15 @@ const StyledDashboardLayout = styled.div`
 `;
 
 function DashboardLayout() {
+  const { bookings, isLoading: isRecentBookingLoading } = useRecentBookings();
+  const {
+    stays,
+    confirmedStays,
+    isLoading: isRecentStaysLoading,
+  } = useRecentStays();
+
+  if (isRecentBookingLoading || isRecentStaysLoading) return <Spinner />;
+
   return (
     <StyledDashboardLayout>
       <div>Statistics</div>
