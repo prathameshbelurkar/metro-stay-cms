9370fd389cf5fc50bdb82b6068d660635a7fb2ca
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2024-06-30 13:13:19 +0530

    dashboard: add area chart (#25)

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -7,6 +7,7 @@ import { useRecentStays } from "./useRecentStays";
 import { useCabins } from "../cabins/useCabins";
 
 import Stats from "./Stats";
+import SalesChart from "./SalesChart";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -38,7 +39,7 @@ function DashboardLayout() {
       />
       <div>Today&apos;s Activity</div>
       <div>Chart stay duration</div>
-      <div>Chart sales</div>
+      <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
   );
 }
