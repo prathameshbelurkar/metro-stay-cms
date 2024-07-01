0612cc252400867d6fbe77f968a46b227cfb578d
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2024-07-01 07:53:45 +0530

    error-boundary: add support for fallback and reset pages (#7)

--- src/ui/ErrorFallback.jsx
+++ src/ui/ErrorFallback.jsx
@@ -1,5 +1,10 @@
 import styled from "styled-components";
 
+import GlobalStyles from "../styles/GlobalStyles";
+
+import Heading from "./Heading";
+import Button from "./Button";
+
 const StyledErrorFallback = styled.main`
   height: 100vh;
   background-color: var(--color-grey-50);
@@ -29,3 +34,22 @@ const Box = styled.div`
     color: var(--color-grey-500);
   }
 `;
+
+function ErrorFallback({ error, resetErrorBoundary }) {
+  return (
+    <>
+      <GlobalStyles />
+      <StyledErrorFallback>
+        <Box>
+          <Heading as="h1">Something went wrong</Heading>
+          <p>{error.message}</p>
+          <Button size="large" onClick={resetErrorBoundary}>
+            Try again
+          </Button>
+        </Box>
+      </StyledErrorFallback>
+    </>
+  );
+}
+
+export default ErrorFallback;
